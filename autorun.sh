481934707158c86858ab633b38db716ea6e1bba2
Jeremy Collins <jeremy@perfectsensedigital.com>
2012-11-14 07:22:41 -0500

    Improve autorun.sh when using brew's libtool on OS X.

--- autorun.sh
+++ autorun.sh
@@ -2,15 +2,16 @@
 
 die() { echo "$@"; exit 1; }
 
-
 aclocal || die "Can't execute aclocal" 
-#autoheader || die "Can't execute autoheader"
 
-if test -f /usr/bin/glibtoolize  ; then
+command -v glibtoolize &> /dev/null
+if [ $? -eq 0 ]; then
   glibtoolize --automake --force || die "Can't execute glibtoolize"
 else
   libtoolize --automake --force || die "Can't execute libtoolize"
 fi
-  
+
 autoconf || die "Can't execute autoconf"
 automake --add-missing --copy --force || die "Can't execute automake"
+
+./configure
