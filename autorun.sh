3d8f5e7c520d8cf07a5dabc8a38d10e87233348a
Jeremy Collins <jeremy@perfectsensedigital.com>
2013-10-09 08:10:45 -0400

    Fix autorun.sh so it works on both OS X (w/brew) and Ubuntu.

--- autorun.sh
+++ autorun.sh
@@ -4,8 +4,7 @@ die() { echo "$@"; exit 1; }
 
 aclocal || die "Can't execute aclocal" 
 
-command -v glibtoolize &> /dev/null
-if [ $? -eq 0 ]; then
+if hash glibtoolize 2> /dev/null; then
   glibtoolize --automake --force || die "Can't execute glibtoolize"
 else
   libtoolize --automake --force || die "Can't execute libtoolize"
