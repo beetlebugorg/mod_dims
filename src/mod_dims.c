c56605a1eadb4990dd78b81f534aedf0219dd627
Rohit S Monga <rmonga@perfectsensedigital.com>
2018-06-07 13:43:42 -0400

    Adds XML header for SVGs, if it doesn't exist.

--- src/mod_dims.c
+++ src/mod_dims.c
@@ -626,6 +626,12 @@ dims_fetch_remote_image(dims_request_rec *d, const char *url)
             return 1;
         }
 
+        // Ensure SVGs have the appropriate XML header.
+        if (strncmp(image_data.data, "<svg", 4) == 0) {
+            image_data.data = apr_pstrcat(d->pool, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n", image_data.data, NULL);
+            image_data.used += 55;
+        }
+
         start_time = apr_time_now();
         if(MagickReadImageBlob(d->wand, image_data.data, image_data.used) 
                 == MagickFalse) {
@@ -642,10 +648,6 @@ dims_fetch_remote_image(dims_request_rec *d, const char *url)
             return 1;
         }
         d->imagemagick_time += (apr_time_now() - start_time) / 1000;
-
-        if(image_data.data) {
-            free(image_data.data);
-        }
 
         if(d->status != DIMS_DOWNLOAD_TIMEOUT) {
             d->original_image_size = image_data.used;
