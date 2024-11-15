cff16945e2193fda28a14f6ad4766a6a3417dd1c
Jeremy Collins <jeremy.collins@beetlebug.org>
2024-11-14 19:53:18 -0500

    Remove uptime from status

--- src/status.c
+++ src/status.c
@@ -5,27 +5,6 @@
 #include <openssl/sha.h>
 #include "mod_dims.h"
 
-static void show_time(request_rec *r, apr_interval_time_t tsecs)
-{
-    int days, hrs, mins, secs;
-
-    secs = (int)(tsecs % 60);
-    tsecs /= 60;
-    mins = (int)(tsecs % 60);
-    tsecs /= 60;
-    hrs = (int)(tsecs % 24);
-    days = (int)(tsecs / 24);
-
-    ap_rprintf(r, "Uptime: ");
-
-    if (days) ap_rprintf(r, " %d day%s", days, days == 1 ? "" : "s");
-    if (hrs) ap_rprintf(r, " %d hour%s", hrs, hrs == 1 ? "" : "s");
-    if (mins) ap_rprintf(r, " %d minute%s", mins, mins == 1 ? "" : "s");
-    if (secs) ap_rprintf(r, " %d second%s", secs, secs == 1 ? "" : "s");
-
-    ap_rprintf(r, "\n");
-}
-
 apr_status_t
 status_handler(request_rec *r) {
     apr_time_t uptime;
@@ -35,8 +14,6 @@ status_handler(request_rec *r) {
 
     uptime = (apr_uint32_t) apr_time_sec(apr_time_now() -
             ap_scoreboard_image->global->restart_time);
-
-    show_time(r, uptime);
 
     ap_rprintf(r, "Restart time: %s\n", 
             ap_ht_time(r->pool,
